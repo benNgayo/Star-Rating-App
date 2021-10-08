7ff3a75a88985e1667713e27f26e5a967eac7c79
benNgayo <56467737+benNgayo@users.noreply.github.com>
2021-10-08 03:26:47 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+import React, { useState } from "react";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({ selected = false, onSelect = (f) => f }) => (
+  <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
+);
+
+const createArray = (length) => [...Array(length)];
+
+export default function App({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(3);
+  return (
+    <div className="App">
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </div>
+  );
+}
